8ec445c8457453dbed1f32a8139be37ffeaefa22
kchangela7 <62969855+kchangela7@users.noreply.github.com>
2020-07-08 12:22:05 -0400

    changing readme

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,6 @@ import Authenticate from './components/Authentication/Authenticate';
 import LoadingContextProvider from './contexts/LoadingContext';
 import Loading from './shared/Loading';
 
-/* A React website created for quickly calculating PCR MasterMix amounts and generating
-a wells layout by taking in simple raw data (ie. sample numbers and control amounts). Backend built
-using Firebase to add Authentication and Database access for user to create their own mix ratios.
-This is my first project created during the summer of 2020 after taking the Intro to CS (Comp 110) at UNC 
-during the Spring 2020 semester. I work in a genetics lab at UNC run by Dr. Koller, the PI of the lab. 
-I built this for myself and others to use in the lab to aid in research. */
-
 function App() {
   return (
     <div className="App">
