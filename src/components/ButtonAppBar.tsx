15affc38f0c4548294959082c69de04fe446565d
kchangela7 <kchangela7@gmail.com>
2021-02-20 14:54:47 -0500

    added functioning drawer

--- src/components/ButtonAppBar.tsx
+++ src/components/ButtonAppBar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { AuthContext } from '../contexts/AuthContext';
 import { auth } from '../firebase';
+import { Box, Drawer, List, ListItem, ListItemText } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -28,21 +29,32 @@ export default function ButtonAppBar() {
   const classes = useStyles();
   const { user, openAuth } = useContext(AuthContext);
 
+  const [drawer, setDrawer] = useState(false);
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton edge="start" onClick={() => setDrawer(true)} className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
             Lab Calculator
           </Typography>
-          { user ? 
+          {user ?
             <Button color="inherit" onClick={() => auth().signOut()}>Sign Out</Button> :
-            <Button color="inherit" onClick={openAuth}>Sign In</Button> 
+            <Button color="inherit" onClick={openAuth}>Sign In</Button>
           }
         </Toolbar>
+        <Drawer anchor={"left"} open={drawer} onClose={() => setDrawer(false)}>
+          <List>
+            <ListItem button key={"Calculator"} onClick={() => setDrawer(false)}>
+              <Box ml={3} mr={3}>
+                <ListItemText primary={"Calculator"} />
+              </Box>
+            </ListItem>
+          </List>
+        </Drawer>
       </AppBar>
     </div>
   );
